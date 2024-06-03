8ac41556377101b75b45bbc0831f92fb9e238a69
Juan Guillermo Buitrago Calle <64021772+Juanbc2@users.noreply.github.com>
2024-06-02 21:18:05 -0500

    wip

--- contenedor-principal.py
+++ contenedor-principal.py
@@ -1,16 +1,17 @@
+import os
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
 from iot import IoT
 from kivy.core.window import Window
-import os
 
 class Contenedor(App):  # el archivo kv debe llamarse igual que esta clase
     sm = ScreenManager()
-    path = os.path.dirname(os.path.abspath(__file__))
 
     def build(self):
+        # Cambia el directorio de trabajo actual al directorio del script
+        os.chdir(os.path.dirname(os.path.abspath(__file__)))
         Window.size = (1024, 768)
-        self.sm.add_widget(IoT(name='iot', path=self.path))
+        self.sm.add_widget(IoT(name='iot'))
         return self.sm
 
 Contenedor().run()
